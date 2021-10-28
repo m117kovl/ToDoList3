915fe0f4fe507833a3909fe870906f97922e90a8
mary1 <m117kovl@mail.ru>
2021-10-28 23:36:38 +0500

    исправленный

--- ToDoList3/ToDoList3/MainViewModel.cs
+++ ToDoList3/ToDoList3/MainViewModel.cs
@@ -2,87 +2,59 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using ToDoList3.Annotations;
 using Xamarin.Forms;
 
 namespace ToDoList3
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        //    public MainViewModel()
-        //    {
-        //        AddCommand = new Command(AddList);
-        //        TaskList = new ObservableCollection<Task>(); //кошелек
-        //    }
-
-        //    public ObservableCollection<Task> TaskList { get; } // список("массив") из задач    карман
-
-        //    private void AddList(object obj) // положить деньги в кошелек
-        //    {
-        //        var s = obj.ToString();
-        //        TaskList.Add(new Task(s)); // конвертнули деньги в рубли
-        //        OnPropertyChanged(); //обновить внешний вид
-        //    }
-
-
-        //    public ICommand AddCommand { get; }
-        //    public event PropertyChangedEventHandler PropertyChanged;
-
-        //    [NotifyPropertyChangedInvocator]
-        //    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        //    {
-        //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        //    }
-        //}
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public ObservableCollection<Task> TaskList { get; }
-
         public MainViewModel()
         {
-            TaskList = new ObservableCollection<Task>();
-
-            CreateTaskCommand = new Command(CreateTask);
-            DeleteTaskCommand = new Command(DeleteTask);
+            AddCommand = new Command(AddList);
+            DeleteCommand = new Command(DeleteList);
+            TaskList = new ObservableCollection<Task>(); //кошелек
         }
 
-        public ICommand CreateTaskCommand { get; }
-        public ICommand DeleteTaskCommand { get; }
+        public ObservableCollection<Task> TaskList { get; } // список("массив") из задач    карман
 
+        private void AddList(object obj) // положить деньги в кошелек
+        {
+            var s = obj.ToString();
+            TaskList.Add(new Task(s)); // конвертнули деньги в рубли
+            OnPropertyChanged(); //обновить внешний вид
+        }
+
+        private void DeleteList(object obj) // убрать деньги из кошелька
+        {
+            if (obj is Task task)  //если типа таск
+            {
+                TaskList.Remove(task);  //удали его
+            }
+
+            OnPropertyChanged(); //обновить внешний вид
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private void CreateTask(object name)
-        {
-            
-            TaskList.Add(new Task(name.ToString()));
-
-            OnPropertyChanged();
-        }
-
-        private void DeleteTask(object taskObj)
-        {
-            if (taskObj is Task task)
-            {
-                TaskList.Remove(task);
-            }
-
-            OnPropertyChanged();
         }
     }
 
     public class Task : BindableObject
     {
-        public Task(string name)
+        public Task(string name)   
         {
             Name = name;
-            IsComplite = false;
+            IsComplete = false;
         }
 
         public string Name { get; set; }
-        public bool IsComplite { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
